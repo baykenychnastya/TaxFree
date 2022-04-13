d946c295f5d88ed9fd743ad82c4c76820c2c9de9
Asus <baykenych.nastya@gmail.com>
2022-04-13 19:58:05 +0300

    Add project files.

---
+++ TaxFree/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TaxFree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
